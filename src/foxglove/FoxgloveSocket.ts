6dfdb98ab0f56dd08ae4a2bc392f3b51ba675854
Rin Iwai <rin@eyrin.jp>
2024-09-14 00:33:51 +0900

    roslib with foxglove

---
+++ src/foxglove/FoxgloveSocket.ts
@@ -0,0 +1,474 @@
+import { parseRos2idl } from '@foxglove/ros2idl-parser';
+import { parse } from '@foxglove/rosmsg';
+import {
+  MessageReader as Ros1MessageReader,
+  MessageWriter as Ros1MessageWriter,
+} from '@foxglove/rosmsg-serialization';
+import {
+  MessageReader as Ros2MessageReader,
+  MessageWriter as Ros2MessageWriter,
+} from '@foxglove/rosmsg2-serialization';
+import {
+  type Channel,
+  type ConnectionGraphUpdate,
+  FoxgloveClient,
+  type MessageData,
+  type ParameterValues,
+  type Service,
+  type ServiceCallFailure,
+  type ServiceCallResponse,
+} from '@foxglove/ws-protocol';
+import WebSocket from 'isomorphic-ws';
+import type { Ros } from 'roslib';
+
+export class FoxgloveSocket {
+  #client: FoxgloveClient;
+  #ros: Ros & { isConnected: boolean };
+  #isRos2: boolean;
+
+  // Message Readers / Writers
+  #messageReaders = new Map<string, Ros1MessageReader | Ros2MessageReader>();
+  #messageWriters = new Map<string, Ros1MessageWriter | Ros2MessageWriter>();
+
+  // Channels
+  #channelsById = new Map<number, Channel>();
+  #channelsByName = new Map<string, Channel>();
+
+  // Services
+  #servicesById = new Map<number, Service>();
+  #servicesByName = new Map<string, Service>();
+
+  #unadvertiseByName = new Map<string, () => void>();
+  #publishByName = new Map<string, Promise<(msg: unknown) => void>>();
+  #unsubscribeByName = new Map<string, () => void>();
+
+  #callId = 0;
+
+  constructor(url: string, ros: Ros, isRos2 = true) {
+    this.#client = new FoxgloveClient({
+      ws: new WebSocket(url, [FoxgloveClient.SUPPORTED_SUBPROTOCOL]),
+    });
+    this.#ros = ros;
+    this.#isRos2 = isRos2;
+
+    // topics
+    this.#client.on('advertise', (channels) => {
+      for (const channel of channels) {
+        this.#channelsById.set(channel.id, channel);
+        this.#channelsByName.set(channel.topic, channel);
+      }
+    });
+    this.#client.on('unadvertise', (channelIds) => {
+      for (const channelId of channelIds) {
+        const channel = this.#channelsById.get(channelId);
+        if (channel) {
+          this.#channelsById.delete(channel.id);
+          this.#channelsByName.delete(channel.topic);
+        }
+      }
+    });
+
+    // services
+    this.#client.on('advertiseServices', (services) => {
+      for (const service of services) {
+        this.#servicesById.set(service.id, service);
+        this.#servicesByName.set(service.name, service);
+      }
+    });
+    this.#client.on('unadvertiseServices', (serviceIds) => {
+      for (const serviceId of serviceIds) {
+        const service = this.#servicesById.get(serviceId);
+        if (service) {
+          this.#servicesById.delete(service.id);
+          this.#servicesByName.delete(service.name);
+        }
+      }
+    });
+
+    // roslib
+    this.#client.on('open', () => {
+      this.#ros.isConnected = true;
+      this.#ros.emit('connection', undefined);
+    });
+
+    this.#client.on('close', (event) => {
+      this.#ros.isConnected = false;
+      this.#ros.emit('close', event);
+    });
+
+    this.#client.on('error', (event) => {
+      this.#ros.emit('error', event);
+    });
+  }
+
+  close() {
+    this.#client.close();
+  }
+
+  send(message: { op: string }) {
+    if (message.op === 'advertise') {
+      this.#advertise(
+        message as unknown as {
+          topic: string;
+          type: string;
+        },
+      );
+    } else if (message.op === 'unadvertise') {
+      this.#unadvertise(
+        message as unknown as {
+          topic: string;
+        },
+      );
+    } else if (message.op === 'publish') {
+      this.#publish(
+        message as unknown as {
+          topic: string;
+          msg: unknown;
+        },
+      );
+    } else if (message.op === 'subscribe') {
+      this.#subscribe(
+        message as unknown as {
+          topic: string;
+        },
+      );
+    } else if (message.op === 'unsubscribe') {
+      this.#unsubscribe(
+        message as unknown as {
+          topic: string;
+        },
+      );
+    } else if (message.op === 'call_service') {
+      this.#callService(
+        message as unknown as {
+          id: string;
+          service: string;
+          type: string;
+          args: unknown;
+        },
+      );
+    } else {
+      console.error('not implemented', message);
+    }
+  }
+
+  #advertise(message: { topic: string; type: string }) {
+    const publisherId = this.#client.advertise({
+      topic: message.topic,
+      encoding: this.#isRos2 ? 'cdr' : 'ros1',
+      schemaName: message.type,
+    });
+    const unadvertise = () => {
+      this.#client.unadvertise(publisherId);
+    };
+    const publish = new Promise<(msg: unknown) => void>((resolve) => {
+      this.#getChannel(message.topic).then((channel) => {
+        const writer = this.#getMessageWriter(channel);
+        resolve((msg: unknown) => {
+          this.#client.sendMessage(publisherId, writer.writeMessage(msg));
+        });
+      });
+    });
+    this.#unadvertiseByName.set(message.topic, unadvertise);
+    this.#publishByName.set(message.topic, publish);
+  }
+
+  #unadvertise(message: { topic: string }) {
+    const unadvertise = this.#unadvertiseByName.get(message.topic);
+    if (unadvertise) {
+      unadvertise();
+    }
+    this.#unadvertiseByName.delete(message.topic);
+  }
+
+  async #publish(message: { topic: string; msg: unknown }) {
+    const publish = await this.#publishByName.get(message.topic);
+    if (publish) {
+      publish(message.msg);
+    }
+  }
+
+  async #subscribe(message: { topic: string }) {
+    const channel = await this.#getChannel(message.topic);
+    const subscriptionId = this.#client.subscribe(channel.id);
+    const listener = (event: MessageData) => {
+      if (event.subscriptionId === subscriptionId) {
+        const reader = this.#getMessageReader(channel);
+        this.#ros.emit(message.topic, reader.readMessage(event.data));
+      }
+    };
+    const unsubscribe = () => {
+      this.#client.off('message', listener);
+      this.#client.unsubscribe(subscriptionId);
+    };
+    this.#unsubscribeByName.set(message.topic, unsubscribe);
+    this.#client.on('message', listener);
+  }
+
+  #unsubscribe(message: { topic: string }) {
+    const unsubscribe = this.#unsubscribeByName.get(message.topic);
+    if (unsubscribe) {
+      unsubscribe();
+    }
+    this.#unsubscribeByName.delete(message.topic);
+  }
+
+  async #callService(message: {
+    id: string;
+    service: string;
+    type: string;
+    args: unknown;
+  }) {
+    if (message.type === 'rosapi/GetParam') {
+      this.#getParam(
+        message as {
+          id: string;
+          args: {
+            name: string;
+          };
+        },
+      );
+    } else if (message.type === 'rosapi/SetParam') {
+      this.#setParam(
+        message as {
+          id: string;
+          args: {
+            name: string;
+            value: string;
+          };
+        },
+      );
+    } else if (message.type === 'rosapi/Topics') {
+      this.#getTopics(message);
+    } else if (message.type === 'rosapi/Services') {
+      this.#getServices(message);
+    } else if (message.type === 'rosapi/TopicType') {
+      this.#getTopicType(
+        message as {
+          id: string;
+          args: {
+            topic: string;
+          };
+        },
+      );
+    } else if (message.type === 'rosapi/ServiceType') {
+      this.#getServiceType(
+        message as {
+          id: string;
+          args: {
+            service: string;
+          };
+        },
+      );
+    } else if (message.type.startsWith('rosapi/')) {
+      console.error('not implemented', message);
+    }
+
+    const service = await this.#getService(message.service);
+    const writer = this.#getMessageWriter(service);
+    const reader = this.#getMessageReader(service);
+    const callId = this.#callId++;
+
+    const listener = (event: ServiceCallResponse) => {
+      if (event.serviceId === service.id && event.callId === callId) {
+        this.#client.off('serviceCallResponse', listener);
+        this.#ros.emit(message.id, {
+          values: reader.readMessage(event.data),
+          result: true,
+        });
+      }
+    };
+    this.#client.on('serviceCallResponse', listener);
+
+    const failureListener = (event: ServiceCallFailure) => {
+      if (event.serviceId === service.id && event.callId === callId) {
+        this.#client.off('serviceCallFailure', failureListener);
+        this.#ros.emit(message.id, { values: event.message, result: false });
+      }
+    };
+    this.#client.on('serviceCallFailure', failureListener);
+
+    this.#client.sendServiceCallRequest({
+      serviceId: service.id,
+      callId,
+      encoding: this.#isRos2 ? 'cdr' : 'ros1',
+      data: new DataView(writer.writeMessage(message.args).buffer),
+    });
+  }
+
+  #getParam(message: { id: string; args: { name: string } }) {
+    const name = message.args.name.replace(':', '.');
+    const listener = (event: ParameterValues) => {
+      if (event.parameters[0]?.name === name && event.id === message.id) {
+        this.#client.off('parameterValues', listener);
+        this.#ros.emit(message.id, {
+          values: { value: JSON.stringify(event.parameters[0].value) },
+          result: true,
+        });
+      }
+    };
+    this.#client.on('parameterValues', listener);
+    this.#client.getParameters([name], message.id);
+  }
+
+  #setParam(message: {
+    id: string;
+    args: { name: string; value: string };
+  }) {
+    const name = message.args.name.replace(':', '.');
+    const listener = (event: ParameterValues) => {
+      if (event.parameters[0]?.name === name && event.id === message.id) {
+        this.#client.off('parameterValues', listener);
+        this.#ros.emit(message.id, { result: true });
+      }
+    };
+    this.#client.on('parameterValues', listener);
+    this.#client.setParameters(
+      [{ name, value: JSON.parse(message.args.value) }],
+      message.id,
+    );
+  }
+
+  #getTopics(message: { id: string }) {
+    const topics = {
+      topics: [...this.#channelsByName.keys()],
+      types: [...this.#channelsByName.values()].map((x) => x.schemaName),
+    };
+    this.#ros.emit(message.id, { values: topics, result: true });
+  }
+
+  #getServices(message: { id: string }) {
+    const listener = (event: ConnectionGraphUpdate) => {
+      this.#client.off('connectionGraphUpdate', listener);
+      this.#client.unsubscribeConnectionGraph();
+      this.#ros.emit(message.id, {
+        values: {
+          services: event.advertisedServices.map((service) => service.name),
+        },
+        result: true,
+      });
+    };
+    this.#client.on('connectionGraphUpdate', listener);
+    this.#client.subscribeConnectionGraph();
+  }
+
+  #getTopicType(message: { id: string; args: { topic: string } }) {
+    const topicType = this.#channelsByName.get(message.args.topic)?.schemaName;
+    if (topicType) {
+      this.#ros.emit(message.id, { values: { type: topicType }, result: true });
+    } else {
+      this.#ros.emit(message.id, { result: false });
+    }
+  }
+
+  #getServiceType(message: { id: string; args: { service: string } }) {
+    const serviceType = this.#servicesByName.get(message.args.service)?.type;
+    if (serviceType) {
+      this.#ros.emit(message.id, {
+        values: { type: serviceType },
+        result: true,
+      });
+    } else {
+      this.#ros.emit(message.id, { result: false });
+    }
+  }
+
+  async #getChannel(name: string) {
+    return (
+      this.#channelsByName.get(name) ??
+      (await new Promise<Channel>((resolve) => {
+        const listener = (channels: Channel[]) => {
+          const channel = channels.find((channel) => channel.topic === name);
+          if (channel) {
+            this.#client.off('advertise', listener);
+            resolve(channel);
+          }
+        };
+        this.#client.on('advertise', listener);
+      }))
+    );
+  }
+
+  async #getService(name: string) {
+    return (
+      this.#servicesByName.get(name) ??
+      (await new Promise<Service>((resolve) => {
+        const listener = (services: Service[]) => {
+          const service = services.find((service) => service.name === name);
+          if (service) {
+            this.#client.off('advertiseServices', listener);
+            resolve(service);
+          }
+        };
+        this.#client.on('advertiseServices', listener);
+      }))
+    );
+  }
+
+  #getMessageReader(channelOrService: Channel | Service) {
+    const name =
+      'schema' in channelOrService
+        ? channelOrService.schemaName
+        : channelOrService.type;
+    return (
+      this.#messageReaders.get(name) ??
+      (() => {
+        const schema =
+          'schema' in channelOrService
+            ? channelOrService.schema
+            : (channelOrService.response?.schema ??
+              channelOrService.responseSchema);
+        const schemaEncoding =
+          'schema' in channelOrService
+            ? channelOrService.schemaEncoding
+            : channelOrService.response?.schemaEncoding;
+        if (typeof schema !== 'string') {
+          console.log(channelOrService);
+          throw new Error('schema not found');
+        }
+        const reader = this.#isRos2
+          ? new Ros2MessageReader(
+              schemaEncoding === 'ros2idl'
+                ? parseRos2idl(schema)
+                : parse(schema, { ros2: true }),
+            )
+          : new Ros1MessageReader(parse(schema, { ros2: false }));
+        this.#messageReaders.set(name, reader);
+        return reader;
+      })()
+    );
+  }
+
+  #getMessageWriter(channelOrService: Channel | Service) {
+    const name =
+      'schema' in channelOrService
+        ? channelOrService.schemaName
+        : channelOrService.type;
+    return (
+      this.#messageWriters.get(name) ??
+      (() => {
+        const schema =
+          'schema' in channelOrService
+            ? channelOrService.schema
+            : (channelOrService.request?.schema ??
+              channelOrService.requestSchema);
+        const schemaEncoding =
+          'schema' in channelOrService
+            ? channelOrService.schemaEncoding
+            : channelOrService.request?.schemaEncoding;
+        if (typeof schema !== 'string') {
+          throw new Error('schema not found');
+        }
+        const writer = this.#isRos2
+          ? new Ros2MessageWriter(
+              schemaEncoding === 'ros2idl'
+                ? parseRos2idl(schema)
+                : parse(schema, { ros2: true }),
+            )
+          : new Ros1MessageWriter(parse(schema, { ros2: false }));
+        this.#messageWriters.set(name, writer);
+        return writer;
+      })()
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,130 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import ROSLIB from 'roslib';
+import { createRos } from './foxglove';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const ros = createRos({});
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// If there is an error on the backend, an 'error' emit will be emitted.
+ros.on('error', (error) => {
+  console.log(error);
+});
+
+// Find out exactly when we made a connection.
+ros.on('connection', () => {
+  console.log('Connection made!');
+});
+
+ros.on('close', () => {
+  console.log('Connection closed.');
+});
+
+// Create a connection to the rosbridge WebSocket server.
+ros.connect('ws://localhost:8765');
+
+// Publishing a Topic
+// ------------------
+
+// First, we create a Topic object with details of the topic's name and message type.
+const cmdVel = new ROSLIB.Topic({
+  ros: ros,
+  name: '/cmd_vel',
+  messageType: 'geometry_msgs/Twist',
+});
+
+// Then we create the payload to be published. The object we pass in to ros.Message matches the
+// fields defined in the geometry_msgs/Twist.msg definition.
+const twist = {
+  linear: {
+    x: 0.1,
+    y: 0.2,
+    z: 0.3,
+  },
+  angular: {
+    x: -0.1,
+    y: -0.2,
+    z: -0.3,
+  },
+};
+
+// And finally, publish.
+cmdVel.publish(twist);
+
+// Subscribing to a Topic
+// ----------------------
+
+// Like when publishing a topic, we first create a Topic object with details of the topic's name
+// and message type. Note that we can call publish or subscribe on the same topic object.
+const listener = new ROSLIB.Topic<{ data: string }>({
+  ros: ros,
+  name: '/listener',
+  messageType: 'std_msgs/String',
+});
+
+// Then we add a callback to be called every time a message is published on this topic.
+listener.subscribe((message) => {
+  console.log(`Received message on ${listener.name}: ${message.data}`);
+
+  // If desired, we can unsubscribe from the topic as well.
+  listener.unsubscribe();
+});
+
+// Calling a service
+// -----------------
+
+// First, we create a Service client with details of the service's name and service type.
+const addTwoIntsClient = new ROSLIB.Service({
+  ros: ros,
+  name: '/add_two_ints',
+  serviceType: 'example_interfaces/AddTwoInts',
+});
+
+// Then we create a Service Request. The object we pass in to ROSLIB.ServiceRequest matches the
+// fields defined in the rospy_tutorials AddTwoInts.srv file.
+const request = {
+  a: 1,
+  b: 2,
+};
+
+// Finally, we call the /add_two_ints service and get back the results in the callback. The result
+// is a ROSLIB.ServiceResponse object.
+addTwoIntsClient.callService(request, (result) => {
+  console.log(
+    `Result for service call on ${addTwoIntsClient.name}: ${result.sum}`,
+  );
+});
+
+// Advertising a Service
+// ---------------------
+
+// The Service object does double duty for both calling and advertising services
+const setBoolServer = new ROSLIB.Service({
+  ros: ros,
+  name: '/set_bool',
+  serviceType: 'std_srvs/SetBool',
+});
+
+// Use the advertise() method to indicate that we want to provide this service
+setBoolServer.advertise((request, response) => {
+  console.log(
+    `Received service request on ${setBoolServer.name}: ${request.data}`,
+  );
+  response.success = true;
+  response.message = 'Set successfully';
+  return true;
+});
+
+// Getting a param value
+// ---------------------
+
+// In ROS 2, params are set in the format 'node_name:param_name'
+const favoriteColor = new ROSLIB.Param({
+  ros: ros,
+  name: '/add_two_ints_server:use_sim_time',
+});
+
+favoriteColor.get((value) => {
+  console.log(`The value of use_sim_time before setting is ${value}`);
+});
+favoriteColor.set(true, () => {});
+favoriteColor.get((value) => {
+  console.log(`The value of use_sim_time after setting is ${value}`);
+});
